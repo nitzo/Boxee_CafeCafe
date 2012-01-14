6aab9a1dd619aaac64e7cf72af4d34363d4371b3
Nitzan Bar <nitzanbar@gmail.com>
2012-01-14 19:25:16 +0200

    Initial Boxee CafeCafe App Commit

---
+++ start.py
@@ -0,0 +1,17 @@
+import mc
+
+
+
+'''
+Pre App Load Stuff
+'''
+
+#TODO: Add Preload Code (Load video play list & play)
+
+
+
+'''
+Activate Splash Screen + Main Window
+'''
+mc.ActivateWindow(14000)	#Splash Screen
+mc.ActivateWindow(14001)	#Main Application Window
