8aa571540a19d130692b3d9c361e0d5798634f75
Nitzan Bar <nitzanbar@gmail.com>
2012-01-16 00:08:26 +0200

    Fixed Bug with Ads Scroller Thread. Thread now exits when closing app

--- main.py
+++ main.py
@@ -13,20 +13,18 @@ def onload():
 	mc.GetWindow(14001).GetList(120).SetContentURL("rss://"+xmlpath)
 	
 	#Create JobManager (For Ads Scrolling)
-	myJobManager = jobmanager.BoxeeJobManager(10,60)
+	myJobManager = jobmanager.BoxeeJobManager(2)
 	
 	#Add Ads Scroll Job To JobManager
 	myJob = jobs.ContainerScrollJob(10, mc.GetWindow(14001).GetList(120))
 	myJobManager.addJob(myJob)
 	
 	#Start Job Manager
-	#myJobManager.run()
+	myJobManager.start()
 	
 
 def onunload():
 	global myJobManager
-	mc.LogDebug("Closing window !!16:25!!")
-	#TODO: Make this Work!!
 	myJobManager.stop()	
 	
 	
