2f3896cf2a31994db85a6842385975c869504899
Nitzan Bar <nitzanbar@gmail.com>
2012-01-17 11:20:15 +0200

    Added Left and Right List containers

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ import jobmanager
 import jobs
 
 myJobManager = 0
+rssRoot = "www.macam.ac.il/nitzan/Boxee/"
 
 def onload():
 	global myJobManager
-	
+	global rssRoot
+		
 	#Set Ads Container Path & Load Content
-	xmlpath = "www.macam.ac.il/nitzan/Boxee/ads.xml"
+	xmlpath = rssRoot + "ads.xml"
 	mc.ShowDialogNotification(xmlpath)
 	mc.GetWindow(14000).GetList(120).SetContentURL("rss://"+xmlpath)
+	
+	#Set Menu Contaier Path
+	xmlpath = rssRoot + "menu1.xml"
+	mc.GetWindow(14000).GetList(130).SetContentURL("rss://"+xmlpath)
 	
 	#Create JobManager (For Ads Scrolling)
 	myJobManager = jobmanager.BoxeeJobManager(2)
