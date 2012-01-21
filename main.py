a6055fd58139a954a2cc3b65b3ac49b4e5cf2c01
Nitzan Bar <nitzanbar@gmail.com>
2012-01-21 17:23:43 +0200

    1. Organized Directory Structure. (Added FTP_data folder for all ftp files) 2. When left menu scrolls update right menu's content url

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 import mc
 import jobmanager
 import jobs
+import utils
 
 myJobManager = 0
 rssRoot = "www.macam.ac.il/nitzan/Boxee/"
 
+adsList = 0
+leftMenuList = 0
+rightMenuList = 0
+
 def onload():
-	global myJobManager
-	global rssRoot
-		
+	global myJobManager, rssRoot, adsList, leftMenuList, rightMenuList
+
+	#Set Global Lists
+	adsList = mc.GetActiveWindow().GetList(120)
+	rightMenuList = mc.GetActiveWindow().GetList(130)
+	leftMenuList = mc.GetActiveWindow().GetList(140)
+
+	
 	#Set Ads Container Path & Load Content
-	xmlpath = rssRoot + "ads.xml"
-	#mc.ShowDialogNotification(xmlpath)
-	mc.GetWindow(14000).GetList(120).SetContentURL("rss://"+xmlpath)
-	
-	#Set Right Menu Container Path
-	xmlpath = rssRoot + "menu1.xml"
-	mc.GetWindow(14000).GetList(130).SetContentURL("rss://"+xmlpath)
-	
+	adsList.SetContentURL("rss://"+rssRoot+"Ads/ads.xml")
+		
 	#Set Left Menu Container Path
-	xmlpath = rssRoot + "menus.xml"
-	mc.GetWindow(14000).GetList(140).SetContentURL("rss://"+xmlpath)
+	leftMenuList.SetContentURL("rss://"+rssRoot+"Menus/menus.xml")
 	
-	#Create JobManager (For Ads Scrolling)
+	#Scroll Right Menu To First Item
+	scrollMenu()
+	
+	#Create JobManager
 	myJobManager = jobmanager.BoxeeJobManager(2)
 	
 	#Add Ads Scroll Job To JobManager
-	myJob = jobs.ContainerScrollJob(10, mc.GetWindow(14000).GetList(120))
+	myJob = jobs.ContainerScrollJob(10, adsList)
 	myJobManager.addJob(myJob)
 	
 	#Start Job Manager
@@ -36,5 +42,28 @@ def onload():
 def onunload():
 	global myJobManager
 	myJobManager.stop()	
+
+#Scroll Left & Right Menus according to scrollDirection
+def scrollMenu(scrollDirection="none"):
+	global leftMenuList, rightMenuList
+	
+	#Scroll Menu List
+	{
+		"left" : leftMenuList.ScrollPageUp,
+		"right": leftMenuList.ScrollPageDown,
+		"none" : leftMenuList.GetFocusedItem		#Do Nothing
+	}[scrollDirection]()
+		
+		
+	#Get Focused ListItem
+	focusedItem = utils.GetFocusedListItem(leftMenuList)
+	
+	#focusedItem.Dump()
+	#mc.ShowDialogNotification(focusedItem.GetPath())
+	
+	#Set Right Menu Content Url Based on focusedItem
+	rightMenuList.SetContentURL(focusedItem.GetPath())
+	
+
 	
 	
