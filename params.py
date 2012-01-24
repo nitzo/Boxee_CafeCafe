d8706be469bdb50b4074928ad52c7368f1371f1c
Nitzan Bar <nitzanbar@gmail.com>
2012-01-24 21:44:00 +0200

    Added Commerials for 1st Pilot Day

--- params.py
+++ params.py
@@ -5,12 +5,12 @@
 adSource = "local" #Use "local" or "rss"
 
 #If adSource is "local" this defines ads list
-localAdList = ["danceforever.gif", "Ad1_Tisan.png", "Ad1_Tisan_Blue.png"]	
+localAdList = ["Gykir.gif", "lexidale.gif", "sonic.gif", "OurAdvertisment.gif"]	
 
 # Durations and Intervals ###
 
 #Interval for switching bottom page ads
-adInterval = 10	
+adInterval = 25	
 #Interval to show Breaking News (Full Screen Ad)	
 breakingNewsInterval = 60
 
