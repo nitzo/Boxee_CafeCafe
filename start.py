e45cbe303b2b9eac29b135334d0de154b68988de
menachem <menachem12345@gmail.com>
2012-01-24 11:29:20 +0200

    Update files

--- start.py
+++ start.py
@@ -14,6 +14,6 @@ Pre App Load Stuff
 Activate Splash Screen + Main Window
 '''
 #mc.ActivateWindow(14001)	#Splash Screen
-#mc.ActivateWindow(14000)	#Main Application Window
-mc.ActivateWindow(14002)	#Main Application Window (With Video)
+mc.ActivateWindow(14000)	#Main Application Window
+#mc.ActivateWindow(14002)	#Main Application Window (With Video)
 
