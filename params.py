bb1bdf1c6e323ebd16b217acc4d162166949cfb2
Nitzan Bar <nitzanbar@gmail.com>
2012-02-26 16:36:38 +0200

    Added Chocolate Cake Full Screen Ad Added QRCode to Our Advertisment

--- params.py
+++ params.py
@@ -5,15 +5,15 @@
 adSource = "local" #Use "local" or "rss"
 
 #If adSource is "local" this defines ads list
-localAdList = ["banner_dance_phoneYakir.gif", "lexidale.gif", "sonic.gif", "banner_out_phoneGuy.gif", "OurAdvertisment.gif", "TomVedan.gif"]	
+localAdList = ["banner_dance_phoneYakir.gif", "lexidale.gif", "OurAdvertisment.gif", "sonic.gif", "banner_out_phoneGuy.gif", "TomVedan.gif"]	
 
 # Durations and Intervals ###
 
 #Interval for switching bottom page ads
 adInterval = 25	
 #Interval to show Breaking News (Full Screen Ad)	
-breakingNewsInterval = 60
-breakingNewsImageList = ["FullScreenAd_Soup.png", "FullScreenAd_Salad.png", "FullScreenAd_Wafel.png" ]
+breakingNewsInterval = 45
+breakingNewsImageList = ["ChocolateCake.png", "FullScreenAd_Soup.png", "ChocolateCake.png", "FullScreenAd_Salad.png", "ChocolateCake.png", "FullScreenAd_Wafel.png" ]
 
 #Duration for braking news dialog
 breakingNewsDuration = 15
